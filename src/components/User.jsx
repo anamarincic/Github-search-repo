7e1e9c3517083e034ecb8b802f44ef4f90e859a2
anamarincic <amarincic5@gmail.com>
2022-07-14 16:51:01 +0200

    img url

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -34,7 +34,7 @@ export function User (props) {
 
     return (
         <div>
-            <img src={props.data.avatar_url}></img>
+            <img src={props.data.avatar_url} alt=""></img>
             <h3>Name: {props.data.name}</h3>
             <h3>Bio: {props.data.bio}</h3>
             <h3>Location: {props.data.location}</h3>
